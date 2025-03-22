166f004f1d721cf96a3650bcac79568a28110734
Vishal Namdeo <vishalkhairagarh@gmail.com>
2025-03-22 16:15:52 +0530

    minor bug fix

--- src/main/java/com/springboot/login/LoginUserApplication.java
+++ src/main/java/com/springboot/login/LoginUserApplication.java
@@ -1,10 +1,14 @@
 package com.springboot.login;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoginUserApplication {
+
+	Dotenv dotenv = Dotenv.configure().load();
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginUserApplication.class, args);
