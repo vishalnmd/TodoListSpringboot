2362d011c6b490ea74c211c5552ec8f6a8648865
Vishal Namdeo <vishalkhairagarh@gmail.com>
2025-03-22 16:38:03 +0530

    minor bug fix

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ FROM maven:3.8.6-eclipse-temurin-17 AS build
 WORKDIR /app
 COPY pom.xml .
 COPY src ./src
-RUN mvn clean package
+RUN mvn clean package -DskipTests
 
 FROM openjdk:17-jdk-alpine
 WORKDIR /app
--- src/main/java/com/springboot/login/LoginUserApplication.java
+++ src/main/java/com/springboot/login/LoginUserApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginUserApplication {
 
-	Dotenv dotenv = Dotenv.configure().load();
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(LoginUserApplication.class, args);
 	}
