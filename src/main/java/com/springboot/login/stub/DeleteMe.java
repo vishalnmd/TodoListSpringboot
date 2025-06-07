acf4daeaa9c7af11c398894e5ca7218c8e2f3c07
Vishal Namdeo <vishalkhairagarh@gmail.com>
2025-06-07 13:23:40 +0530

    add .env for db credentials

--- src/main/java/com/springboot/login/stub/DeleteMe.java
+++ src/main/java/com/springboot/login/stub/DeleteMe.java
@@ -1,23 +1,33 @@
-package com.springboot.login.stub;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class DeleteMe {
-    public static void main(String args[]){
-        String req = "{\"request\":\"{\\\"id\\\":1,\\\"task\\\":\\\"play cricket\\\"}\"}";
-
-        try{
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(req);
-            JsonNode jsonNode1 = objectMapper.readTree(jsonNode.get("request").asText());
-
-            System.out.println(jsonNode.get("request").asText());
-            System.out.println(jsonNode1.get("task").asText());
-
-        }catch (Exception e){
-            System.out.println(e.toString());
-        }
-    }
-}
+//package com.springboot.login.stub;
+//
+//import com.fasterxml.jackson.databind.JsonNode;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class DeleteMe implements CommandLineRunner {
+//
+//    Logger logger = LoggerFactory.getLogger(DeleteMe.class);
+//
+//    @Value("${spring.datasource.username}")
+//    private String url;
+//
+//    @Value("${spring.datasource.url}")
+//    private String dbUrl;
+//
+//    public static void main(String args[]){
+//        SpringApplication.run(DeleteMe.class,args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        System.out.println(url);
+//        logger.info(url);
+//        logger.info(dbUrl);
+//    }
+//}
